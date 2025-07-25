9c5ad089234d4f2ed4b78a6faf889a4eba984bd3
Abhimanyu14 <abhimanyu.n14@gmail.com>
2025-07-25 16:32:52 +0530

    [TECH] FakeClipboardKitImpl

---
+++ codebase/android/barcodes/src/main/java/com/makeappssimple/abhimanyu/barcodes/android/core/common/clipboard/fake/FakeClipboardKitImpl.kt
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2025-2025 Abhimanyu
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.makeappssimple.abhimanyu.barcodes.android.core.common.clipboard.fake
+
+import android.content.Context
+import androidx.annotation.VisibleForTesting
+import com.makeappssimple.abhimanyu.barcodes.android.core.common.clipboard.ClipboardKit
+
+internal class FakeClipboardKitImpl : ClipboardKit {
+    // Pair<label, text>
+    @VisibleForTesting
+    internal val copiedTexts = mutableListOf<Pair<String, String>>()
+
+    override fun copyToClipboard(
+        context: Context,
+        label: String,
+        text: String
+    ): Boolean {
+        copiedTexts.add(
+            element = Pair(
+                first = label,
+                second = text
+            ),
+        )
+        return true
+    }
+}
