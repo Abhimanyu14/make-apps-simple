31cafce7bcd49062a315f12e62595977d4de49da
Abhimanyu14 <abhimanyu.n14@gmail.com>
2025-07-29 00:02:07 +0530

    [TEST] CreateBarcodeScreenUITest

---
+++ codebase/android/barcodes/src/androidTest/java/com/makeappssimple/abhimanyu/barcodes/android/feature/barcodedetails/barcodedetails/screen/CreateBarcodeScreenUITest.kt
@@ -0,0 +1,307 @@
+/*
+ * Copyright 2025-2025 Abhimanyu
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.makeappssimple.abhimanyu.barcodes.android.feature.barcodedetails.barcodedetails.screen
+
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.test.ExperimentalTestApi
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.hasText
+import androidx.compose.ui.test.isFocused
+import androidx.compose.ui.test.junit4.ComposeContentTestRule
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.onRoot
+import androidx.compose.ui.test.printToLog
+import com.makeappssimple.abhimanyu.barcodes.android.core.designsystem.theme.MyAppTheme
+import com.makeappssimple.abhimanyu.barcodes.android.feature.createbarcode.createbarcode.screen.CreateBarcodeScreenUI
+import com.makeappssimple.abhimanyu.barcodes.android.feature.createbarcode.createbarcode.state.CreateBarcodeScreenUIState
+import org.junit.Before
+import org.junit.Ignore
+import org.junit.Rule
+import org.junit.Test
+
+@OptIn(ExperimentalTestApi::class)
+internal class CreateBarcodeScreenUITest {
+    @get:Rule
+    val composeTestRule: ComposeContentTestRule = createComposeRule()
+
+    private val testBarcodeName = "test-barcode-name"
+    private val testBarcodeValue = "test-barcode-value"
+
+    private lateinit var testUtil: CreateBarcodeScreenUITestUtil
+
+    @Before
+    fun setUp() {
+        testUtil = CreateBarcodeScreenUITestUtil(
+            composeTestRule = composeTestRule,
+        )
+    }
+
+    // region default state
+    @Test
+    fun createBarcodeScreenUIElementsAreDisplayed_byDefault() = runUITest {
+        setScreenContent()
+
+        assertTopAppBarIsDisplayed()
+        assertTopAppBarNavigationButtonIsDisplayed()
+        assertTopAppBarTitleTextIsMatching()
+        assertBarcodeNameTextFieldIsDisplayed()
+        assertBarcodeValueTextFieldIsDisplayed()
+        assertCopyBarcodeValueButtonDoesNotExist()
+        assertCtaButtonIsDisplayed()
+    }
+
+    @Test
+    fun createBarcodeScreenUIContentIsScrollable_byDefault() = runUITest {
+        setScreenContent()
+
+        assertRootNodeIsScrollable()
+    }
+    // endregion
+
+    // region clear text buttons
+    @Test
+    fun clearBarcodeNameButtonIsDisplayed_whenBarcodeNameTextFieldIsNotBlank() =
+        runUITest {
+            setScreenContent(
+                barcodeName = testBarcodeName,
+            )
+
+            assertClearBarcodeNameButtonIsDisplayed()
+        }
+
+    @Test
+    fun clearBarcodeValueButtonIsDisplayed_whenBarcodeValueTextFieldIsNotBlank() =
+        runUITest {
+            setScreenContent(
+                barcodeValue = testBarcodeValue,
+            )
+
+            assertClearBarcodeValueButtonIsDisplayed()
+        }
+    // endregion
+
+    // region cta button state
+    @Test
+    fun ctaButtonDisabled_byDefault() = runUITest {
+        setScreenContent()
+
+        assertCtaButtonIsNotEnabled()
+    }
+
+    @Test
+    fun ctaButtonEnabled_whenBarcodeIsValid() =
+        runUITest {
+            setScreenContent(
+                isBarcodeValid = true,
+            )
+
+            assertCtaButtonIsEnabled()
+        }
+    // endregion
+
+    // region isBarcodeValueEditable = false
+    @Test
+    fun clearBarcodeValueButtonDoesNotExist_whenIsBarcodeValueEditableIsFalse() =
+        runUITest {
+            setScreenContent(
+                barcodeValue = testBarcodeValue,
+                isBarcodeValueEditable = false,
+            )
+
+            performBarcodeValueTextFieldClick()
+            assertClearBarcodeValueButtonDoesNotExist()
+        }
+
+    @Test
+    fun copyBarcodeValueButtonDoesNotExist_whenIsBarcodeValueEditableIsFalse() =
+        runUITest {
+            setScreenContent(
+                barcodeValue = testBarcodeValue,
+                isBarcodeValueEditable = false,
+            )
+
+            assertCopyBarcodeValueButtonIsDisplayed()
+        }
+
+    @Test
+    @Ignore("https://issuetracker.google.com/issues/317345954")
+    fun barcodeValueTextFieldIsNotEditableIfIsBarcodeValueEditableIsFalse() =
+        runUITest {
+            setScreenContent(
+                barcodeValue = testBarcodeValue,
+                isBarcodeValueEditable = false,
+            )
+
+            assertBarcodeValueTextFieldIsNotEditable()
+        }
+    // endregion
+
+    // region autofocus
+    @Test
+    fun barcodeNameTextFieldIsFocused_byDefault() = runUITest {
+        setScreenContent()
+
+        composeTestRule.waitUntilNodeCount(isFocused(), 1)
+        assertBarcodeNameTextFieldIsFocused()
+    }
+    // endregion
+
+    // region text input
+    @Test
+    @Ignore("https://issuetracker.google.com/issues/317432140")
+    fun barcodeNameTextFieldHasGivenText_whenUserEntersText() = runUITest {
+        setScreenContent()
+
+        composeTestRule.waitUntilNodeCount(isFocused(), 1)
+        performBarcodeNameTextFieldTextInput(
+            text = testBarcodeName,
+        )
+        composeTestRule.waitForIdle()
+        composeTestRule.waitUntilNodeCount(
+            hasText(testBarcodeName),
+            1
+        )
+        assertBarcodeNameTextFieldHasText(
+            text = testBarcodeName,
+        )
+    }
+
+    @Test
+    @Ignore("https://issuetracker.google.com/issues/317432140")
+    fun barcodeValueTextFieldHasGivenText_whenUserEntersText() = runUITest {
+        setScreenContent()
+
+        composeTestRule.waitUntilNodeCount(isFocused(), 1)
+        performBarcodeValueTextFieldTextInput(
+            text = testBarcodeValue,
+        )
+        composeTestRule.waitForIdle()
+        assertBarcodeValueTextFieldHasText(
+            text = testBarcodeValue,
+        )
+    }
+    // endregion
+
+    // region ime
+    @Test
+    fun barcodeValueTextFieldIsFocusedOnBarcodeNameTextFieldImeAction_whenIsBarcodeValueEditableIsTrue() =
+        runUITest {
+            setScreenContent(
+                barcodeValue = testBarcodeValue,
+                isBarcodeValueEditable = true,
+            )
+
+            composeTestRule.waitUntilNodeCount(isFocused(), 1)
+            assertBarcodeNameTextFieldIsFocused()
+            performBarcodeNameTextFieldImeAction()
+            composeTestRule.waitForIdle()
+            assertBarcodeNameTextFieldIsNotFocused()
+            assertBarcodeValueTextFieldIsFocused()
+        }
+
+    @Test
+    fun barcodeValueTextFieldIsNotFocusedOnBarcodeNameTextFieldImeAction_whenIsBarcodeValueEditableIsFalse() =
+        runUITest {
+            setScreenContent(
+                barcodeName = testBarcodeName,
+                barcodeValue = testBarcodeValue,
+                isBarcodeValueEditable = false,
+            )
+
+            composeTestRule.waitUntilNodeCount(isFocused(), 1)
+            assertBarcodeNameTextFieldIsFocused()
+            performBarcodeNameTextFieldImeAction()
+            assertBarcodeNameTextFieldIsFocused()
+            assertBarcodeValueTextFieldIsNotFocused()
+        }
+    // endregion
+
+    // region end to end flow
+    @Test
+    @Ignore("https://issuetracker.google.com/issues/317432140")
+    fun barcodeCreationCompleteFlow() = runUITest {
+        val (barcodeName, setBarcodeName) = mutableStateOf("")
+        val (barcodeValue, setBarcodeValue) = mutableStateOf("")
+
+        setScreenContent(
+            barcodeValue = barcodeValue,
+            barcodeName = barcodeName,
+            isBarcodeValueEditable = true,
+        )
+
+        performBarcodeNameTextFieldTextInput(
+            text = testBarcodeName,
+        )
+        composeTestRule
+            .onNodeWithText(testBarcodeName)
+            .assertIsDisplayed()
+
+        assertClearBarcodeNameButtonIsDisplayed()
+
+        performBarcodeNameTextFieldImeAction()
+        assertBarcodeNameTextFieldIsNotFocused()
+        assertBarcodeValueTextFieldIsFocused()
+        assertCtaButtonIsNotEnabled()
+
+        performBarcodeValueTextFieldTextInput(
+            text = testBarcodeValue,
+        )
+
+        composeTestRule.onRoot().printToLog("currentLabelExists")
+
+        composeTestRule
+            .onNodeWithText(testBarcodeValue)
+            .assertIsDisplayed()
+        assertClearBarcodeValueButtonIsDisplayed()
+
+        performBarcodeValueTextFieldImeAction()
+        assertBarcodeValueTextFieldIsNotFocused()
+        assertCtaButtonIsEnabled()
+    }
+    // endregion
+
+    private fun setScreenContent(
+        isBarcodeValid: Boolean = false,
+        isBarcodeValueEditable: Boolean = true,
+        barcodeName: String = "",
+        barcodeValue: String = "",
+    ) {
+        composeTestRule.setContent {
+            MyAppTheme {
+                CreateBarcodeScreenUI(
+                    uiState = CreateBarcodeScreenUIState(
+                        isBarcodeValid = isBarcodeValid,
+                        isBarcodeValueEditable = isBarcodeValueEditable,
+                        barcodeName = barcodeName,
+                        barcodeValue = barcodeValue,
+                    ),
+                )
+            }
+        }
+    }
+
+    private fun runUITest(
+        block: CreateBarcodeScreenUITestUtil.() -> Unit,
+    ) {
+        with(
+            receiver = testUtil,
+        ) {
+            block()
+        }
+    }
+}
---
+++ codebase/android/barcodes/src/androidTest/java/com/makeappssimple/abhimanyu/barcodes/android/feature/barcodedetails/barcodedetails/screen/CreateBarcodeScreenUITestUtil.kt
@@ -0,0 +1,239 @@
+/*
+ * Copyright 2025-2025 Abhimanyu
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.makeappssimple.abhimanyu.barcodes.android.feature.barcodedetails.barcodedetails.screen
+
+import androidx.compose.ui.test.SemanticsNodeInteraction
+import androidx.compose.ui.test.assert
+import androidx.compose.ui.test.assertHasClickAction
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.assertIsEnabled
+import androidx.compose.ui.test.assertIsFocused
+import androidx.compose.ui.test.assertIsNotEnabled
+import androidx.compose.ui.test.assertIsNotFocused
+import androidx.compose.ui.test.assertTextEquals
+import androidx.compose.ui.test.hasScrollAction
+import androidx.compose.ui.test.hasText
+import androidx.compose.ui.test.junit4.ComposeContentTestRule
+import androidx.compose.ui.test.onNodeWithContentDescription
+import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
+import androidx.compose.ui.test.performImeAction
+import androidx.compose.ui.test.performTextInput
+import com.makeappssimple.abhimanyu.barcodes.android.core.common.constants.TestTags
+
+internal class CreateBarcodeScreenUITestUtil(
+    private val composeTestRule: ComposeContentTestRule,
+) {
+    private val barcodeNameTextFieldLabel = "Barcode Name"
+    private val barcodeValueTextFieldLabel = "Barcode Value"
+    private val clearBarcodeNameButtonContentDescription = "Clear barcode name"
+    private val clearBarcodeValueButtonContentDescription =
+        "Clear barcode value"
+    private val copyBarcodeValueButtonContentDescription = "Copy barcode value"
+    private val ctaButtonLabel = "SAVE"
+    private val topAppbarTitleText = "Enter Barcode Details"
+
+    // region Finders
+    private fun findRootNode(): SemanticsNodeInteraction {
+        return composeTestRule.onNodeWithTag(
+            testTag = TestTags.SCREEN_CONTENT_CREATE_BARCODE,
+            useUnmergedTree = true,
+        )
+    }
+
+    private fun findBarcodeNameTextField(): SemanticsNodeInteraction {
+        return composeTestRule.onNodeWithText(
+            text = barcodeNameTextFieldLabel,
+        )
+    }
+
+    private fun findBarcodeValueTextField(): SemanticsNodeInteraction {
+        return composeTestRule.onNodeWithText(
+            text = barcodeValueTextFieldLabel,
+        )
+    }
+
+    private fun findClearBarcodeNameButton(): SemanticsNodeInteraction {
+        return composeTestRule.onNodeWithContentDescription(
+            label = clearBarcodeNameButtonContentDescription,
+        )
+    }
+
+    private fun findClearBarcodeValueButton(): SemanticsNodeInteraction {
+        return composeTestRule.onNodeWithContentDescription(
+            label = clearBarcodeValueButtonContentDescription,
+        )
+    }
+
+    private fun findCopyBarcodeValueButton(): SemanticsNodeInteraction {
+        return composeTestRule.onNodeWithContentDescription(
+            label = copyBarcodeValueButtonContentDescription,
+        )
+    }
+
+    private fun findCtaButton(): SemanticsNodeInteraction {
+        return composeTestRule.onNodeWithText(
+            text = ctaButtonLabel,
+        )
+    }
+
+    private fun findTopAppBar(): SemanticsNodeInteraction {
+        return composeTestRule.onNodeWithTag(
+            testTag = TestTags.COMPONENT_MY_TOP_APP_BAR,
+        )
+    }
+
+    private fun findTopAppBarNavigationButton(): SemanticsNodeInteraction {
+        return composeTestRule.onNodeWithTag(
+            testTag = TestTags.COMPONENT_MY_TOP_APP_BAR_NAVIGATION_BUTTON,
+        )
+    }
+
+    private fun findTopAppBarTitleText(): SemanticsNodeInteraction {
+        return composeTestRule.onNodeWithTag(
+            testTag = TestTags.COMPONENT_MY_TOP_APP_BAR_TITLE_TEXT,
+        )
+    }
+    // endregion
+
+    // region Actions
+    internal fun performBarcodeNameTextFieldClick() {
+        findBarcodeNameTextField().assertHasClickAction()
+        findBarcodeNameTextField().performClick()
+    }
+
+    internal fun performBarcodeNameTextFieldImeAction() {
+        findBarcodeNameTextField().performImeAction()
+    }
+
+    internal fun performBarcodeNameTextFieldTextInput(
+        text: String,
+    ) {
+        findBarcodeNameTextField().performTextInput(text)
+    }
+
+    internal fun performBarcodeValueTextFieldClick() {
+        findBarcodeValueTextField().assertHasClickAction()
+        findBarcodeValueTextField().performClick()
+    }
+
+    internal fun performBarcodeValueTextFieldImeAction() {
+        findBarcodeValueTextField().performImeAction()
+    }
+
+    internal fun performBarcodeValueTextFieldTextInput(
+        text: String,
+    ) {
+        findBarcodeValueTextField().performTextInput(text)
+    }
+    // endregion
+
+    // region Assertions
+    internal fun assertBarcodeNameTextFieldHasText(
+        text: String,
+    ) {
+        findBarcodeNameTextField().assert(hasText(text))
+    }
+
+    internal fun assertBarcodeNameTextFieldIsDisplayed() {
+        findBarcodeNameTextField().assertIsDisplayed()
+    }
+
+    internal fun assertBarcodeNameTextFieldIsFocused() {
+        findBarcodeNameTextField().assertIsFocused()
+    }
+
+    internal fun assertBarcodeNameTextFieldIsNotFocused() {
+        findBarcodeNameTextField().assertIsNotFocused()
+    }
+
+    internal fun assertBarcodeValueTextFieldHasText(
+        text: String,
+    ) {
+        findBarcodeValueTextField().assert(hasText(text))
+    }
+
+    internal fun assertBarcodeValueTextFieldIsDisplayed() {
+        findBarcodeValueTextField().assertIsDisplayed()
+    }
+
+    internal fun assertBarcodeValueTextFieldIsNotEnabled() {
+        findBarcodeValueTextField().assertIsNotEnabled()
+    }
+
+    internal fun assertBarcodeValueTextFieldIsNotEditable() {
+        // TODO-Abhi: Compose testing does not support this yet.
+    }
+
+    internal fun assertBarcodeValueTextFieldIsFocused() {
+        findBarcodeValueTextField().assertIsFocused()
+    }
+
+    internal fun assertBarcodeValueTextFieldIsNotFocused() {
+        findBarcodeValueTextField().assertIsNotFocused()
+    }
+
+    internal fun assertClearBarcodeNameButtonIsDisplayed() {
+        findClearBarcodeNameButton().assertIsDisplayed()
+    }
+
+    internal fun assertClearBarcodeValueButtonDoesNotExist() {
+        findClearBarcodeValueButton().assertDoesNotExist()
+    }
+
+    internal fun assertClearBarcodeValueButtonIsDisplayed() {
+        findClearBarcodeValueButton().assertIsDisplayed()
+    }
+
+    internal fun assertCopyBarcodeValueButtonDoesNotExist() {
+        findCopyBarcodeValueButton().assertDoesNotExist()
+    }
+
+    internal fun assertCopyBarcodeValueButtonIsDisplayed() {
+        findCopyBarcodeValueButton().assertIsDisplayed()
+    }
+
+    internal fun assertCtaButtonIsDisplayed() {
+        findCtaButton().assertIsDisplayed()
+    }
+
+    internal fun assertCtaButtonIsEnabled() {
+        findCtaButton().assertIsEnabled()
+    }
+
+    internal fun assertCtaButtonIsNotEnabled() {
+        findCtaButton().assertIsNotEnabled()
+    }
+
+    internal fun assertRootNodeIsScrollable() {
+        findRootNode().assert(hasScrollAction())
+    }
+
+    internal fun assertTopAppBarIsDisplayed() {
+        findTopAppBar().assertIsDisplayed()
+    }
+
+    internal fun assertTopAppBarNavigationButtonIsDisplayed() {
+        findTopAppBarNavigationButton().assertIsDisplayed()
+    }
+
+    internal fun assertTopAppBarTitleTextIsMatching() {
+        findTopAppBarTitleText().assertTextEquals(topAppbarTitleText)
+    }
+    // endregion
+}
