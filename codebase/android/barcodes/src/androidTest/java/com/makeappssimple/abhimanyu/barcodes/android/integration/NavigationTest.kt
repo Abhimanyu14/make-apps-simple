ee7e8a882aeccaaa2738ae0e3ded08eec60bdec7
Abhimanyu14 <abhimanyu.n14@gmail.com>
2025-11-22 21:36:54 +0530

    (BARCODES) [TECH] navigation test

--- codebase/android/barcodes/src/androidTest/java/com/makeappssimple/abhimanyu/barcodes/android/integration/NavigationTest.kt
+++ codebase/android/barcodes/src/androidTest/java/com/makeappssimple/abhimanyu/barcodes/android/integration/NavigationTest.kt
@@ -17,21 +17,27 @@
 package com.makeappssimple.abhimanyu.barcodes.android.integration
 
 import androidx.compose.ui.test.ExperimentalTestApi
-import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.assertHasClickAction
 import androidx.compose.ui.test.isDisplayed
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithContentDescription
 import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
 import androidx.test.core.app.ActivityScenario
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.makeappssimple.abhimanyu.barcodes.android.activity.BarcodesActivity
+import com.makeappssimple.abhimanyu.barcodes.android.core.constants.TestTags.SCREEN_CREATE_BARCODE
+import com.makeappssimple.abhimanyu.barcodes.android.core.constants.TestTags.SCREEN_CREDITS
 import com.makeappssimple.abhimanyu.barcodes.android.core.constants.TestTags.SCREEN_HOME
 import com.makeappssimple.abhimanyu.barcodes.android.core.constants.TestTags.SCREEN_SETTINGS
+import com.makeappssimple.abhimanyu.barcodes.android.core.constants.TestTags.SCREEN_WEB_VIEW
 import com.makeappssimple.abhimanyu.barcodes.android.di.BarcodesAppModule
 import com.makeappssimple.abhimanyu.barcodes.android.test.KoinTestRule
 import kotlinx.coroutines.test.StandardTestDispatcher
+import org.junit.After
 import org.junit.Before
+import org.junit.Ignore
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -59,19 +65,71 @@ class NavigationTest {
         effectContext = testCoroutineDispatcher,
     )
 
+    private var activityScenario: ActivityScenario<BarcodesActivity>? = null
+
     @Before
     fun setUp() {
-        ActivityScenario.launch(
+        activityScenario = ActivityScenario.launch(
             BarcodesActivity::class.java,
         )
     }
 
+    @After
+    fun tearDown() {
+        activityScenario?.close()
+    }
+
     @Test
-    fun settingsNavigationTest() {
+    fun settingsScreenNavigationTest() {
         assertHomeScreenIsDisplayed()
         assertSettingsIconIsDisplayed()
         clickSettingsIcon()
         assertSettingsScreenIsDisplayed()
+    }
+
+    @Test
+    @Ignore("To Fix")
+    fun creditsScreenNavigationTest() {
+        assertHomeScreenIsDisplayed()
+        assertSettingsIconIsDisplayed()
+        clickSettingsIcon()
+        assertCreditsListItemIsDisplayed()
+        clickCreditsListItem()
+        assertCreditsScreenIsDisplayed()
+    }
+
+    @Test
+    @Ignore("To Fix")
+    fun openSourceLicensesScreenNavigationTest() {
+        assertHomeScreenIsDisplayed()
+        assertSettingsIconIsDisplayed()
+        clickSettingsIcon()
+        assertOpenSourceLicensesListItemIsDisplayed()
+        clickOpenSourceLicensesListItem()
+        assertOpenSourceLicensesScreenIsDisplayed()
+    }
+
+    @Test
+    @Ignore("To Fix")
+    fun privacyPolicyScreenNavigationTest() {
+        assertHomeScreenIsDisplayed()
+        assertSettingsIconIsDisplayed()
+        clickSettingsIcon()
+        assertPrivacyPolicyListItemIsDisplayed()
+        clickPrivacyPolicyListItem()
+        assertPrivacyPolicyScreenIsDisplayed()
+    }
+
+    @Test
+    @Ignore("To Fix")
+    fun createBarcodeScreenNavigationTest() {
+        assertHomeScreenIsDisplayed()
+        assertAddFloatingActionButtonIsDisplayed()
+        clickAddFloatingActionButton()
+        assertCreateBarcodeBottomSheetListItemIsDisplayed()
+        assertCreateBarcodeBottomSheetListItemHasClickAction()
+        clickCreateBarcodeBottomSheetListItem()
+        assertCreateBarcodeScreenIsDisplayed()
     }
 
     private fun clickSettingsIcon() {
@@ -92,19 +150,172 @@ class NavigationTest {
         }
     }
 
-    private fun assertHomeScreenIsDisplayed() {
+    private fun assertAddFloatingActionButtonIsDisplayed() {
+        composeTestRule.waitUntil {
+            composeTestRule
+                .onNodeWithContentDescription(
+                    label = "Add",
+                )
+                .isDisplayed()
+        }
+    }
+
+    private fun assertCreateBarcodeBottomSheetListItemIsDisplayed() {
+        composeTestRule.waitUntil {
+            composeTestRule
+                .onNodeWithText(
+                    "Create barcode"
+                )
+                .isDisplayed()
+        }
+    }
+
+    private fun assertCreateBarcodeBottomSheetListItemHasClickAction() {
         composeTestRule
-            .onNodeWithTag(
-                testTag = SCREEN_HOME,
+            .onNodeWithText(
+                "Create barcode"
             )
-            .assertIsDisplayed()
+            .assertHasClickAction()
+    }
+
+    private fun assertCreditsListItemIsDisplayed() {
+        composeTestRule.waitUntil {
+            composeTestRule
+                .onNodeWithText(
+                    "Credits"
+                )
+                .isDisplayed()
+        }
+    }
+
+    private fun assertPrivacyPolicyListItemIsDisplayed() {
+        composeTestRule.waitUntil {
+            composeTestRule
+                .onNodeWithText(
+                    "Privacy Policy"
+                )
+                .isDisplayed()
+        }
+    }
+
+    private fun assertOpenSourceLicensesListItemIsDisplayed() {
+        composeTestRule.waitUntil {
+            composeTestRule
+                .onNodeWithText(
+                    "Open Source Licenses"
+                )
+                .isDisplayed()
+        }
+    }
+
+    private fun clickCreditsListItem() {
+        composeTestRule
+            .onNodeWithText(
+                "Credits"
+            )
+            .performClick()
+    }
+
+    private fun clickPrivacyPolicyListItem() {
+        composeTestRule
+            .onNodeWithText(
+                "Privacy Policy"
+            )
+            .performClick()
+    }
+
+    private fun clickOpenSourceLicensesListItem() {
+        composeTestRule
+            .onNodeWithText(
+                "Open Source Licenses"
+            )
+            .performClick()
+    }
+
+    private fun clickCreateBarcodeBottomSheetListItem() {
+        composeTestRule
+            .onNodeWithText(
+                "Create barcode"
+            )
+            .isDisplayed()
+    }
+
+    private fun clickAddFloatingActionButton() {
+        composeTestRule
+            .onNodeWithContentDescription(
+                label = "Add",
+            )
+            .performClick()
+    }
+
+    private fun assertHomeScreenIsDisplayed() {
+        composeTestRule.waitUntil {
+            composeTestRule
+                .onNodeWithTag(
+                    testTag = SCREEN_HOME,
+                )
+                .isDisplayed()
+        }
     }
 
     private fun assertSettingsScreenIsDisplayed() {
-        composeTestRule
-            .onNodeWithTag(
-                testTag = SCREEN_SETTINGS,
-            )
-            .assertIsDisplayed()
+        composeTestRule.waitUntil {
+            composeTestRule
+                .onNodeWithTag(
+                    testTag = SCREEN_SETTINGS,
+                )
+                .isDisplayed()
+        }
+    }
+
+    private fun assertCreateBarcodeScreenIsDisplayed() {
+        composeTestRule.waitUntil(
+            timeoutMillis = 5_000,
+        ) {
+            composeTestRule
+                .onNodeWithTag(
+                    testTag = SCREEN_CREATE_BARCODE,
+                )
+                .isDisplayed()
+        }
+    }
+
+    private fun assertCreditsScreenIsDisplayed() {
+        composeTestRule.waitUntil {
+            composeTestRule
+                .onNodeWithTag(
+                    testTag = SCREEN_CREDITS,
+                )
+                .isDisplayed()
+        }
+    }
+
+    private fun assertOpenSourceLicensesScreenIsDisplayed() {
+        composeTestRule.waitUntil {
+            composeTestRule
+                .onNodeWithText(
+                    text = "Open Source Licenses"
+                )
+                .isDisplayed()
+        }
+    }
+
+    private fun assertPrivacyPolicyScreenIsDisplayed() {
+        composeTestRule.waitUntil {
+            composeTestRule
+                .onNodeWithTag(
+                    testTag = SCREEN_WEB_VIEW,
+                )
+                .isDisplayed()
+        }
+        composeTestRule.waitUntil(
+            timeoutMillis = 5_000,
+        ) {
+            composeTestRule
+                .onNodeWithText(
+                    text = "Privacy Policy"
+                )
+                .isDisplayed()
+        }
     }
 }
