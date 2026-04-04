bc65aaf1c0cacd71eb311691249c87b5bf587204
Abhimanyu14 <abhimanyu.n14@gmail.com>
2026-04-04 08:51:40 +0530

    (MAKEAPPSSIMPLE) [TECH] SVG Icon fixes

--- codebase/kmp/composeApp/src/commonMain/kotlin/com/makeappssimple/abhimanyu/kmp/ui/components/Cursor.kt
+++
@@ -1,5 +0,0 @@
-package com.makeappssimple.abhimanyu.kmp.ui.components
-
-import androidx.compose.ui.Modifier
-
-expect fun Modifier.cursorPointer(): Modifier
--- codebase/kmp/composeApp/src/jvmMain/kotlin/com/makeappssimple/abhimanyu/kmp/ui/components/Cursor.kt
+++
@@ -1,7 +0,0 @@
-package com.makeappssimple.abhimanyu.kmp.ui.components
-
-import androidx.compose.ui.Modifier
-
-actual fun Modifier.cursorPointer(): Modifier {
-    return this
-}
