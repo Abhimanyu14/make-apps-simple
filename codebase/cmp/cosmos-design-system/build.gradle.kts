1971d1d3e205744122a2b8497efbda0b391efe2b
Abhimanyu14 <abhimanyu.n14@gmail.com>
2026-05-28 10:34:01 +0530

    (ALL) [TECH] cosmos design system resources

--- codebase/cmp/cosmos-design-system/build.gradle.kts
+++ codebase/cmp/cosmos-design-system/build.gradle.kts
@@ -65,6 +65,13 @@ kotlin {
         commonMain {
             dependencies {
                 implementation(libs.kotlin.stdlib)
+                implementation(libs.compose.components.resources)
+                implementation(libs.compose.foundation)
+                implementation(libs.compose.material3)
+                implementation(libs.compose.runtime)
+                implementation(libs.compose.ui)
+                implementation(libs.compose.ui.tooling.preview)
+                implementation(libs.kotlinx.collections.immutable)
             }
         }
 
