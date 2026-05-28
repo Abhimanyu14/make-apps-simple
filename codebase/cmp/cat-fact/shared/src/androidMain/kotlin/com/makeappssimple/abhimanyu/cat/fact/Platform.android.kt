805198f76e58bde79ef75834e9bd54fe7b8f0f27
Abhimanyu14 <abhimanyu.n14@gmail.com>
2026-05-28 18:49:26 +0530

    (ALL) [TECH] copyrights and formatting

--- codebase/cmp/cat-fact/shared/src/androidMain/kotlin/com/makeappssimple/abhimanyu/cat/fact/Platform.android.kt
+++ codebase/cmp/cat-fact/shared/src/androidMain/kotlin/com/makeappssimple/abhimanyu/cat/fact/Platform.android.kt
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2025-2026 Abhimanyu
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.makeappssimple.abhimanyu.cat.fact
 
 import android.os.Build
@@ -6,4 +22,6 @@ private class AndroidPlatform : Platform {
     override val name: String = "Android ${Build.VERSION.SDK_INT}"
 }
 
-internal actual fun getPlatform(): Platform = AndroidPlatform()
+internal actual fun getPlatform(): Platform {
+    return AndroidPlatform()
+}
--- codebase/cmp/cosmos-design-system-catalog/shared/src/jsMain/kotlin/com/makeappssimple/abhimanyu/cosmos/design/system/catalog/Platform.js.kt
+++ codebase/cmp/cosmos-design-system-catalog/shared/src/jsMain/kotlin/com/makeappssimple/abhimanyu/cosmos/design/system/catalog/Platform.js.kt
@@ -1,16 +1,48 @@
+/*
+ * Copyright 2025-2026 Abhimanyu
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.makeappssimple.abhimanyu.cosmos.design.system.catalog
 
 import web.navigator.navigator
 
 private class JsPlatform : Platform {
     private val userAgent = navigator.userAgent
-    private val browserList = listOf("Chrome", "Firefox", "Safari", "Edge")
+    private val browserList = listOf(
+        "Chrome",
+        "Firefox",
+        "Safari",
+        "Edge",
+    )
 
-    override val name: String = userAgent.findAnyOf(browserList, ignoreCase = true)
+    override val name: String = userAgent
+        .findAnyOf(
+            strings = browserList,
+            ignoreCase = true,
+        )
         ?.let { (startIndex) ->
-            userAgent.substring(startIndex).substringBefore(" ")
+            userAgent
+                .substring(
+                    startIndex = startIndex,
+                ).substringBefore(
+                    delimiter = " ",
+                )
         }
         ?: "Unknown"
 }
 
-internal actual fun getPlatform(): Platform = JsPlatform()
+internal actual fun getPlatform(): Platform {
+    return JsPlatform()
+}
