1fb30ba6f5b659a52cd05621f0e73d486894300f
Abhimanyu14 <abhimanyu.n14@gmail.com>
2026-05-28 23:24:15 +0530

    (ALL) [TECH] date time

---
+++ codebase/cmp/core/date-time/src/wasmJsMain/kotlin/com/makeappssimple/abhimanyu/core/date/time/DateTimeFormatter.wasmJs.kt
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2025-2026 Abhimanyu
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.makeappssimple.abhimanyu.core.date.time
+
+import kotlinx.datetime.TimeZone
+import kotlin.time.Instant
+
+public actual fun formatInstant(
+    instant: Instant,
+    zoneId: TimeZone,
+    pattern: String,
+): String {
+    // TODO(Abhi): This is a fallback for Android to CMP migration
+    // Keep wasm implementation dependency-free and portable.
+    // We intentionally ignore zone/pattern here as a fallback.
+    return instant.toString()
+}
