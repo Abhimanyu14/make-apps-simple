db5d298ea76fbc83db1e55b6c8d5066b9d95d57a
Abhimanyu14 <abhimanyu.n14@gmail.com>
2026-05-29 15:40:52 +0530

    (ALL) [TECH] formatting changes

--- codebase/cmp/cat-fact/desktop-app/src/main/kotlin/com/makeappssimple/abhimanyu/cat/fact/main.kt
+++ codebase/cmp/cat-fact/desktop-app/src/main/kotlin/com/makeappssimple/abhimanyu/cat/fact/main.kt
@@ -19,11 +19,13 @@ package com.makeappssimple.abhimanyu.cat.fact
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 
-internal fun main() = application {
-    Window(
-        onCloseRequest = ::exitApplication,
-        title = "Cat Fact",
-    ) {
-        App()
+internal fun main() {
+    return application {
+        Window(
+            onCloseRequest = ::exitApplication,
+            title = "Cat Fact",
+        ) {
+            App()
+        }
     }
 }
