7cd6369cf8338fb3bd43133369ddeb44f62650f7
Abhimanyu14 <abhimanyu.n14@gmail.com>
2026-06-06 11:20:43 +0530

    (ALL) [TECH] core modules

---
+++ codebase/cmp/core/app-version/build.gradle.kts
@@ -0,0 +1,145 @@
+/*
+ * Copyright 2025-2026 Abhimanyu
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
+// region Lint Config
+val lintConfig: com.android.build.api.dsl.Lint.() -> Unit = {
+    abortOnError = false
+    checkAllWarnings = true
+    htmlReport = true
+    warningsAsErrors = true
+    baseline = file(
+        path = "lint-baseline.xml",
+    )
+    lintConfig = rootProject.file("config/lint/lint.xml")
+
+    // Force-ignore rules that evaluate before lint.xml loads
+    // Prefer adding to lint.xml first
+    disable.addAll(
+        elements = listOf(
+            "AndroidGradlePluginVersion",
+            "NewerVersionAvailable",
+            "Registered",
+        ),
+    )
+}
+// endregion
+
+plugins {
+    alias(libs.plugins.plugin.kotlin.multiplatform)
+    alias(libs.plugins.plugin.kotlin.multiplatform.library)
+    alias(libs.plugins.plugin.android.lint)
+}
+
+kotlin {
+    // region Platforms
+    android {
+        namespace = "com.makeappssimple.abhimanyu.core.app.version"
+        compileSdk {
+            version = release(libs.versions.android.compile.sdk.get().toInt()) {
+                minorApiLevel = 1
+            }
+        }
+        minSdk = libs.versions.android.min.sdk.get().toInt()
+
+        compilerOptions {
+            jvmTarget = JvmTarget.fromTarget(libs.versions.java.get())
+        }
+
+        lint(
+            action = lintConfig,
+        )
+
+        withHostTestBuilder {}
+
+        withDeviceTestBuilder {
+            sourceSetTreeName = "test"
+        }.configure {
+            instrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        }
+    }
+
+    jvm()
+
+    js {
+        browser()
+    }
+
+    listOf(
+        iosArm64(),
+        iosSimulatorArm64()
+    ).forEach { iosTarget ->
+        iosTarget.binaries.framework {
+            baseName = "core:app-version"
+            isStatic = true
+        }
+    }
+
+    @OptIn(ExperimentalWasmDsl::class)
+    wasmJs {
+        browser()
+    }
+    // endregion
+
+    explicitApi()
+
+    sourceSets {
+        androidMain {
+            dependencies {}
+        }
+
+        commonMain {
+            dependencies {
+                implementation(libs.kotlinx.coroutines.core)
+                implementation(libs.koin.core)
+                implementation(libs.koin.annotations)
+                implementation(libs.kotlin.stdlib)
+                implementation(libs.kotlinx.collections.immutable)
+
+                implementation(project(":core:kotlin"))
+            }
+        }
+
+        commonTest {
+            dependencies {
+                implementation(libs.test.kotlin)
+            }
+        }
+
+        getByName("androidDeviceTest") {
+            dependencies {
+                implementation(libs.androidx.test.core)
+                implementation(libs.androidx.test.ext.junit)
+                implementation(libs.androidx.test.runner)
+            }
+        }
+
+        iosMain {
+            dependencies {}
+        }
+    }
+}
+
+// region JVM Lint
+// Apply lint to the JVM/iOS verification targets (Fixes the :updateLintBaselineJvm crash)
+plugins.withId("com.android.lint") {
+    extensions.configure<com.android.build.api.dsl.Lint> {
+        lintConfig()
+    }
+}
+// endregion
